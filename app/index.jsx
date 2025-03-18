037fcdbd98f7f99c5bf236f15d2cd248f1b96d68
saif <saif>
2025-03-18 17:42:11 +0300

    onBoarding Screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,34 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ScrollView } from 'react-native-web';
 
 import { Images } from '../assets/image'
+import {CustomButton } from '../components/CustomButton';
 
 export default function App() {
     return (
         <SafeAreaView className="bg-primary h-full">
-            <ScrollView contentContainerStyle={{ height: '100%'}}>  
-                <View className="w-full justify-center items-center h-full px-4">
-                <Image 
-                source={Images.logo}
-                className="w-[130px] h-[84px]"
-                resizeMode="contain"
-                />
+            <ScrollView contentContainerStyle={{ height: '100%' }}>
+                <View className="w-full justify-center items-center min-h-[84px] px-4">
+                    <Image
+                        source={Images.logo}
+                        className="w-[130px] h-[84px]"
+                        resizeMode="contain"
+                    />
 
-                <Image
-                source={Images.cards}
-                className="max-w-[380px] w-full h-[300px]"
-                resizeMode="contain"
-                />
+                    <Image
+                        source={Images.cards}
+                        className="max-w-[380px] w-full h-[300px]"
+                        resizeMode="contain"
+                    />
 
-                <View className="relative mt-5">
+                    <View className="relative mt-5">
+                        <Text className="text-3xl text-white font-bold text-center">
+                            Discover Endless Possibilities with {' '}
+                            <Text className="text-secondary-200">Aora</Text>
+                        </Text>
 
-                </View>
+                        <Image
+                            source={Images.path}
+                            className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+                            resizeMode="contain"
+                        />
+                    </View>
 
+                    <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovadtion: embark on a 
+                        journy of limitless exploration with Aora</Text>
+
+                        <CustomButton
+                        title="Continue with email"
+                        handlePress={() => router.push('/sign-in')}
+                        containerStyles="w-full mt-7"
+                        />
                 </View>
             </ScrollView>
+            <StatusBar backgroundColor='#161622' style='light'/>
 
         </SafeAreaView>
     );
